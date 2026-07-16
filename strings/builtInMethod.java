b46f442d7d2e374ad1ccce90a5ee0ae1fa1fa69b
harshpratapsinghhh <harshsuryavanshi169@gmail.com>
2026-07-17 01:33:43 +0530

    moreOperations of string

--- strings/builtInMethod.java
+++ strings/builtInMethod.java
@@ -9,7 +9,20 @@ public class builtInMethod {
         System.out.println(s.lastIndexOf('h'));
         System.out.println(s.lastIndexOf('q'));
         // s.toLowerCase()
-
+        System.out.println("LowerCase: "+s.toLowerCase());
         // s.toUpperCase()
+        System.out.println("UpperCase: "+s.toUpperCase());
+        // s.compareTo()
+        String a = "Harsh";
+        String b = "Harshit";
+        String c = "Harsh";
+        System.out.println(a.compareTo(b)); // -2 because it is more in harshit
+        System.out.println(b.compareTo(a)); // 2
+        System.out.println(c.compareTo(a)); // return 0
+        // s1.concat(s2)
+        String s1 = "Thakur ";
+        String s2 = "Sahab";
+        System.out.println("Concat: "+s1.concat(s2));
+
     }
 }
---
+++ strings/moreOperators.java
@@ -0,0 +1,7 @@
+package strings;
+
+public class moreOperators {
+    public static void main(String[] args) {
+        System.out.println("Sum: "+10+20); // no sum it will be added as string
+    }
+}
